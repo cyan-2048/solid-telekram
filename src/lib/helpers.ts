ecff242fd7043abf4b77e33ecca0ef6639ac8ee5
cyan2048 <cyankindasus@protonmail.com>
2024-08-01 21:40:16 +0800

    update mtcute

---
+++ src/lib/helpers.ts
@@ -0,0 +1,41 @@
+export function capitalizeFirstLetter(string: string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function sleep(ms = 0) {
+	return new Promise<void>((resolve) => setTimeout(resolve, ms));
+}
+
+export const cyrb53 = (str: string, seed = 0) => {
+	let h1 = 0xdeadbeef ^ seed,
+		h2 = 0x41c6ce57 ^ seed;
+	for (let i = 0, ch; i < str.length; i++) {
+		ch = str.charCodeAt(i);
+		h1 = Math.imul(h1 ^ ch, 2654435761);
+		h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+	h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+	h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
+export function last<T = any>(arr: T[]) {
+	return arr[arr.length - 1];
+}
+
+// A simple, *insecure* 32-bit hash that's short, fast, and has no dependencies.
+// Output is always 7 characters.
+// Loosely based on the Java version; see
+// https://stackoverflow.com/questions/6122571/simple-non-secure-hash-function-for-javascript
+export const simpleHash = (str: string) => {
+	let hash = 0;
+	for (let i = 0; i < str.length; i++) {
+		const char = str.charCodeAt(i);
+		hash = (hash << 5) - hash + char;
+	}
+	// Convert to 32bit unsigned integer in base 36 and pad with "0" to ensure length is 7.
+	return hash >>> 0;
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,9 +3,7 @@ import scrollIntoViewNPM from "scroll-into-view";
 import { Accessor, createEffect, createSignal, onCleanup } from "solid-js";
 import { Readable, get } from "./stores";
 
-export function capitalizeFirstLetter(string: string) {
-	return string.charAt(0).toUpperCase() + string.slice(1);
-}
+export * from "./helpers";
 
 const keyList = Object.keys;
 
@@ -63,10 +61,6 @@ export function useStore<T, R extends keyof T>(
 	});
 
 	return state;
-}
-
-export function sleep(ms = 0) {
-	return new Promise<void>((resolve) => setTimeout(resolve, ms));
 }
 
 export function centerScroll(el: HTMLElement | Element, sync = false, time = 200) {
@@ -144,24 +138,6 @@ export function scrollIntoView(element: HTMLElement) {
 		element.scrollIntoView(false); // bottom
 	}
 }
-
-export function last<T = any>(arr: T[]) {
-	return arr[arr.length - 1];
-}
-
-// A simple, *insecure* 32-bit hash that's short, fast, and has no dependencies.
-// Output is always 7 characters.
-// Loosely based on the Java version; see
-// https://stackoverflow.com/questions/6122571/simple-non-secure-hash-function-for-javascript
-export const simpleHash = (str: string) => {
-	let hash = 0;
-	for (let i = 0; i < str.length; i++) {
-		const char = str.charCodeAt(i);
-		hash = (hash << 5) - hash + char;
-	}
-	// Convert to 32bit unsigned integer in base 36 and pad with "0" to ensure length is 7.
-	return hash >>> 0;
-};
 
 /**
  *
@@ -361,19 +337,3 @@ export function isSelectionAtStart() {
 
 	return false;
 }
-
-export const cyrb53 = (str: string, seed = 0) => {
-	let h1 = 0xdeadbeef ^ seed,
-		h2 = 0x41c6ce57 ^ seed;
-	for (let i = 0, ch; i < str.length; i++) {
-		ch = str.charCodeAt(i);
-		h1 = Math.imul(h1 ^ ch, 2654435761);
-		h2 = Math.imul(h2 ^ ch, 1597334677);
-	}
-	h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
-	h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
-	h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
-	h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
-
-	return 4294967296 * (2097151 & h2) + (h1 >>> 0);
-};
