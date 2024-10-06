a6b882afb41d67543cf26c10f6a1b4c235ad9afc
cyan2048 <cyankindasus@protonmail.com>
2024-10-07 02:56:48 +0800

    update mtcute layer (0.17.0)

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -3,10 +3,24 @@ import Content from "./components/Content";
 import Header from "./components/Header";
 import styles from "./Settings.module.scss";
 import SpatialNavigation from "@/lib/spatial_navigation";
+import { sleep } from "@/lib/utils";
+import { client } from "@signals";
+import scrollIntoView from "scroll-into-view-if-needed";
 
-function SettingsItem(props: { children: JSXElement }) {
+function SettingsItem(props: { children: JSXElement; onEnterDown?: () => void }) {
 	return (
-		<div tabIndex={-1} class={styles.item}>
+		<div
+			on:sn-enter-down={props.onEnterDown}
+			on:sn-willfocus={(e) => {
+				scrollIntoView(e.currentTarget, {
+					scrollMode: "if-needed",
+					block: "nearest",
+					inline: "nearest",
+				});
+			}}
+			tabIndex={-1}
+			class={styles.item}
+		>
 			{props.children}
 		</div>
 	);
@@ -38,7 +52,27 @@ export default function Settings(props: { onClose: () => void }) {
 				style={{ "background-color": "white", height: "100%" }}
 			>
 				<SettingsItem>Proxy</SettingsItem>
-				<SettingsItem>Logout</SettingsItem>
+				<SettingsItem
+					onEnterDown={async () => {
+						const tg = client()!;
+
+						await sleep(100);
+
+						const sure = confirm("Are you sure you want to logout?");
+
+						if (!sure) return;
+
+						const success = await tg.logOut();
+						if (!success) {
+							alert("Logout was not successful!");
+							await tg.storage.clear(true);
+						}
+						location.reload();
+					}}
+				>
+					Logout
+				</SettingsItem>
+				<SettingsItem>Subscribe Push Notifications</SettingsItem>
 				<SettingsItem>About</SettingsItem>
 			</div>
 		</Content>
