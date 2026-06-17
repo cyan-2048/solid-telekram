b714e5d7123b861a2310eeda48dfc889b0c24ca1
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2026-06-17 18:40:26 +0800

    test release this should work!

--- src/views/DebugView.tsx
+++ src/views/DebugView.tsx
@@ -233,6 +233,7 @@ export default function DebugView() {
 					Cloudphone Mode
 				</CheckboxInput>
 				<Separator>Telegram Icons</Separator>
+				<div>!!</div>
 				<TestAllIcons></TestAllIcons>
 			</div>
 		</Content>
