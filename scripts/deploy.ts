7d62a42ab613a1790ec81434bebe08e6618b6e94
cyan-2048 <72785466+cyan-2048@users.noreply.github.com>
2026-06-17 16:37:46 +0800

    add deploy script

---
+++ scripts/deploy.ts
@@ -0,0 +1,61 @@
+import { resolve } from "path";
+import fs from "fs/promises";
+
+const local = resolve(__dirname, "..", ".env.local");
+
+let localFileContent = "";
+
+// if a local env file is present copy the contents and put it back later
+if (await Bun.file(local).exists()) {
+	localFileContent = await Bun.file(local).text();
+}
+
+// Number.MAX_SAFE_INTEGER - Math.floor(Number.MAX_SAFE_INTEGER / 2)
+const SAFE_INTEGER = 4503599627370496;
+// (BigInt(Math.PI.toFixed(40).replace(".","")) * 4n).toString(16)
+const RANDOM_STRING = "1714add755dc69de3dd72c7aaee15e5aa88";
+
+const TEXT = `VITE_APP_ID=${SAFE_INTEGER}
+VITE_APP_HASH="${RANDOM_STRING}"`;
+
+const IS_PREVIEW = Bun.argv[2] === "--preview";
+
+console.log(IS_PREVIEW);
+
+try {
+	await Bun.write(local, TEXT);
+
+	await Bun.$`git switch latest`;
+
+	// build files
+	await Bun.$`bun run build`;
+	await Bun.$`bun run build:v3`;
+	await Bun.$`bun run build:v4`;
+
+	await Bun.$`git switch gh-pages`;
+
+	if (IS_PREVIEW) {
+		for (let i = 2; i < 5; i++) {
+			const zipFile = resolve(__dirname, "..", "builds", `telekram4kai${i}.zip`);
+			const destination = resolve(__dirname, "..", "preview", `telekram4kai${i}.zip`);
+
+			await Bun.write(destination, Bun.file(zipFile));
+		}
+	} else {
+		const manifestFile = resolve(__dirname, "..", "src", "assets", "manifest.webapp");
+		const appVersion = (await Bun.file(manifestFile).json()).version as string;
+
+		for (let i = 2; i < 5; i++) {
+			const zipFile = resolve(__dirname, "..", "builds", `telekram4kai${i}.zip`);
+			const destination = resolve(__dirname, "..", "v", appVersion, `telekram4kai${i}.zip`);
+
+			await Bun.write(destination, Bun.file(zipFile));
+		}
+	}
+} finally {
+	await Bun.$`git switch latest`;
+
+	if (localFileContent) {
+		await Bun.write(local, localFileContent);
+	}
+}
